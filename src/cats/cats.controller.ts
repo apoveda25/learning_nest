f5a8aadbc6db9041369c03d902a1339f3d371e03
Alfredo Poveda <alfpovsistemas@gmail.com>
2020-05-05 01:20:03 -0400

    Vision general, controladores, codigo de estado HTTP

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Req } from '@nestjs/common';
+import { Controller, Get, Post, Req, HttpCode } from '@nestjs/common';
 import { Request } from 'express';
 
 @Controller('cats')
@@ -9,6 +9,7 @@ export class CatsController {
   }
 
   @Post()
+  @HttpCode(204)
   create(): string {
     return 'This action adds a new cat';
   }
