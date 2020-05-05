6d777a1192986c117c1e48c4ee9d32e90959325d
Alfredo Poveda <alfpovsistemas@gmail.com>
2020-05-05 01:14:51 -0400

    Vision general, controladores, rutas comodines

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -3,6 +3,11 @@ import { Request } from 'express';
 
 @Controller('cats')
 export class CatsController {
+  @Get('ab*cd')
+  find() {
+    return 'This route uses a wildcard';
+  }
+
   @Post()
   create(): string {
     return 'This action adds a new cat';
