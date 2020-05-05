a73ec2d8a4208c9df92ec89322b16e3d1ed05892
Alfredo Poveda <alfpovsistemas@gmail.com>
2020-05-05 01:26:23 -0400

    Vision general, controladores, redireccion

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,8 +1,25 @@
-import { Controller, Get, Post, Req, HttpCode, Header } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Req,
+  HttpCode,
+  Header,
+  Redirect,
+  Query,
+} from '@nestjs/common';
 import { Request } from 'express';
 
 @Controller('cats')
 export class CatsController {
+  @Get('docs')
+  @Redirect('https://docs.nestjs.com', 302)
+  getDocs(@Query('version') version) {
+    if (version && version === '5') {
+      return { url: 'https://docs.nestjs.com/v5/' };
+    }
+  }
+
   @Get('ab*cd')
   find() {
     return 'This route uses a wildcard';
