d51b79e2c589754a4e013f7018b40934e0606f5b
Alfredo Poveda <alfpovsistemas@gmail.com>
2020-05-06 23:29:46 -0400

    Vision general, proveedores, introduccion

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
-import { AdminController } from './admin/admin.controller';
-import { AccountController } from './account/account.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, CatsController, AdminController, AccountController],
+  controllers: [AppController, CatsController],
   providers: [AppService],
 })
 export class AppModule {}
