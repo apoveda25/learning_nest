03c99218f5ec4e4e548f599a5777c801641cff7f
apoveda25 <alfpovsistemas@gmail.com>
2020-05-17 01:37:23 -0400

    Vision general, tuberias, tuberia de validacion de alcance global

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { logger } from './common/middleware/logger.middleware';
+import { ValidationPipe } from './cats/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(logger);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
