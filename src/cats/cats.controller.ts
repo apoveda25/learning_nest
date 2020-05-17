3be2229ac16dac6737f72c28bf0f10ed50ac5bd9
apoveda25 <alfpovsistemas@gmail.com>
2020-05-17 01:31:30 -0400

    Vision general, tuberias, union de tuberia de validacion y cuerpo recibido por un metodo de controlador

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,17 +1,9 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  UseFilters,
-  UsePipes,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, UseFilters } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { CatsService } from './cats.service';
-import { Cat } from './interfaces/cat.interface';
 import { ForbiddenException } from './forbidden.exception';
 import { HttpExceptionFilter } from './http-exception.filter';
-import { JoiValidationPipe } from './validation.pipe';
+import { ValidationPipe } from './validation.pipe';
 
 @UseFilters(HttpExceptionFilter)
 @Controller('cats')
@@ -19,8 +11,7 @@ export class CatsController {
   constructor(private catsService: CatsService) {}
 
   @Post()
-  @UsePipes(new JoiValidationPipe(createCatSchema))
-  async create(@Body() createCatDto: CreateCatDto) {
+  async create(@Body(new ValidationPipe()) createCatDto: CreateCatDto) {
     this.catsService.create(createCatDto);
   }
 
