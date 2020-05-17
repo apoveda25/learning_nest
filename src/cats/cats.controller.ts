c15387c4c14bc9f5899c46065bf6d50e6aafe0a4
apoveda25 <alfpovsistemas@gmail.com>
2020-05-17 01:08:54 -0400

    Vision general, tuberias, union de tuberias y controladores

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get, Post, Body, UseFilters } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  UseFilters,
+  UsePipes,
+} from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { CatsService } from './cats.service';
 import { Cat } from './interfaces/cat.interface';
 import { ForbiddenException } from './forbidden.exception';
 import { HttpExceptionFilter } from './http-exception.filter';
+import { JoiValidationPipe } from './validation.pipe';
 
 @UseFilters(HttpExceptionFilter)
 @Controller('cats')
@@ -11,6 +19,7 @@ export class CatsController {
   constructor(private catsService: CatsService) {}
 
   @Post()
+  @UsePipes(new JoiValidationPipe(createCatSchema))
   async create(@Body() createCatDto: CreateCatDto) {
     this.catsService.create(createCatDto);
   }
